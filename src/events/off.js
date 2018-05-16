5052d1b6c108d9aa0a975bad70c86553c27fc440
Bogdan Chadkin <trysound@yandex.ru>
2018-05-16 21:35:07 +0300

    Treeshake events by lazy caching

--- src/events/off.js
+++ src/events/off.js
@@ -1,16 +1,23 @@
 import canUseDOM from '../util/inDOM'
 
-let off = () => {}
-if (canUseDOM) {
-  off = (function(){
-    if (document.addEventListener)
-      return (node, eventName, handler, capture) =>
-          node.removeEventListener(eventName, handler, capture || false);
+let offCache
 
-    else if (document.attachEvent)
-      return (node, eventName, handler) =>
-          node.detachEvent('on' + eventName, handler);
-  })();
+let off = (node, eventName, handler, capture) => {
+  if (!offCache) {
+    if (canUseDOM) {
+      if (document.addEventListener)
+        offCache = (node, eventName, handler, capture) =>
+          node.removeEventListener(eventName, handler, capture || false)
+
+      else if (document.attachEvent)
+        offCache = (node, eventName, handler) =>
+          node.detachEvent('on' + eventName, handler)
+    } else {
+      offCache = () => {}
+    }
+  }
+
+  return offCache(node, eventName, handler, capture)
 }
 
 export default off
--- src/events/on.js
+++ src/events/on.js
@@ -1,22 +1,28 @@
 import canUseDOM from '../util/inDOM'
 
-let on = () => {}
-if (canUseDOM) {
-  on = (function(){
+let onCache
 
-    if (document.addEventListener)
-      return (node, eventName, handler, capture) =>
-          node.addEventListener(eventName, handler, capture || false);
+let on = (node, eventName, handler, capture) => {
+  if (!onCache) {
+    if (canUseDOM) {
+      if (document.addEventListener)
+        onCache = (node, eventName, handler, capture) =>
+          node.addEventListener(eventName, handler, capture || false)
 
-    else if (document.attachEvent)
-      return (node, eventName, handler) =>
+      else if (document.attachEvent)
+        onCache = (node, eventName, handler) =>
           node.attachEvent('on' + eventName, (e) => {
-            e = e || window.event;
-            e.target = e.target || e.srcElement;
-            e.currentTarget = node;
-            handler.call(node, e);
-          });
-  })();
+            e = e || window.event
+            e.target = e.target || e.srcElement
+            e.currentTarget = node
+            handler.call(node, e)
+          })
+    } else {
+      onCache = () => {}
+    }
+  }
+
+  return onCache(node, eventName, handler, capture)
 }
 
 export default on
