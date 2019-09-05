911fa591438b58824acea5e6e460aa7c8ef41141
Jason Quense <monastic.panic@gmail.com>
2019-09-05 13:21:49 -0400

    feat(scrollParent): allow stopping at the first possible scroll parent (#60)

--- src/scrollParent.ts
+++ src/scrollParent.ts
@@ -4,26 +4,39 @@ import css from './css'
 import height from './height'
 import isDocument from './isDocument'
 
-export default function scrollPrarent(node: HTMLElement) {
-  let position = css(node, 'position')
+/**
+ * Find the first scrollable parent of an element.
+ *
+ * @param element Starting element
+ * @param firstPossible Stop at the first scrollable parent, even if it's not currently scrollable
+ */
+export default function scrollPrarent(
+  element: HTMLElement,
+  firstPossible?: boolean
+) {
+  let position = css(element, 'position')
   let excludeStatic = position === 'absolute'
-  let ownerDoc = node.ownerDocument
+  let ownerDoc = element.ownerDocument
 
   if (position === 'fixed') return ownerDoc || document
 
   // @ts-ignore
-  while ((node = node.parentNode) && !isDocument(node)) {
-    let isStatic = excludeStatic && css(node, 'position') === 'static',
+  while ((element = element.parentNode) && !isDocument(element)) {
+    let isStatic = excludeStatic && css(element, 'position') === 'static',
       style =
-        (css(node, 'overflow') || '') +
-        (css(node, 'overflow-y') || '') +
-        css(node, 'overflow-x')
+        (css(element, 'overflow') || '') +
+        (css(element, 'overflow-y') || '') +
+        css(element, 'overflow-x')
 
     if (isStatic) continue
 
-    if (/(auto|scroll)/.test(style) && height(node) < node!.scrollHeight)
-      return node
+    if (
+      /(auto|scroll)/.test(style) &&
+      (firstPossible || height(element) < element!.scrollHeight)
+    ) {
+      return element
+    }
   }
 
-  return document
+  return ownerDoc || document
 }
